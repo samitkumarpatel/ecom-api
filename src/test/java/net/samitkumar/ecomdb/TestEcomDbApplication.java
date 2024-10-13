575e409e456e81acc4eca1c4a5a9a5acb21b0c03
samitkumarpatel <samitkumarpatel@gmail.com>
2024-10-13 23:29:59 +0200

    enhancement

--- src/test/java/net/samitkumar/ecomdb/TestEcomDbApplication.java
+++ src/test/java/net/samitkumar/ecomdb/TestEcomDbApplication.java
@@ -7,5 +7,4 @@ public class TestEcomDbApplication {
 	public static void main(String[] args) {
 		SpringApplication.from(EcomDbApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
-
 }
