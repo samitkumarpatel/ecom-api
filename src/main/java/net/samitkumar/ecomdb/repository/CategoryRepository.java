39a511598e018a4f5f71ad2e2d2b87eea588d125
samitkumarpatel <samitkumarpatel@gmail.com>
2024-10-18 23:19:51 +0200

    more details added to category and product

--- src/main/java/net/samitkumar/ecomdb/repository/CategoryRepository.java
+++ src/main/java/net/samitkumar/ecomdb/repository/CategoryRepository.java
@@ -1,7 +1,17 @@
 package net.samitkumar.ecomdb.repository;
 
 import net.samitkumar.ecomdb.entity.Category;
+import net.samitkumar.ecomdb.entity.Product;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface CategoryRepository extends ListCrudRepository<Category, Long> {
+import java.util.List;
+
+//@RepositoryRestResource(collectionResourceRel = "category", path = "catalog")
+public interface CategoryRepository extends ListPagingAndSortingRepository<Category, Long>, ListCrudRepository<Category, Long> {
+    @RestResource
+    List<Category> findByName(String name);
+    @RestResource
+    List<Category> findByNameContaining(String name);
 }
--- src/main/java/net/samitkumar/ecomdb/repository/ProductRepository.java
+++ src/main/java/net/samitkumar/ecomdb/repository/ProductRepository.java
@@ -2,9 +2,14 @@ package net.samitkumar.ecomdb.repository;
 
 import net.samitkumar.ecomdb.entity.Product;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends ListCrudRepository<Product, Long> {
+public interface ProductRepository extends ListPagingAndSortingRepository<Product, Long>, ListCrudRepository<Product, Long> {
     List<Product> findByCategory(Long categoryId);
+    List<Product> findByName(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByNameContainingIgnoreCase(String name);
+
 }
